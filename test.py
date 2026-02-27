059399be239e93452a5b49e837ad2f8a56393ea4
hrvoje adzija <hrvoje.adzija@gmail.com>
2026-02-27 11:18:02 +0100

    prvi commit, nema promjena

---
+++ test.py
@@ -0,0 +1,7 @@
+class Instructors:
+    companyName = "Blueline"
+    def __init__(self,course):
+            self.course = course
+
+    def printinfo (self):
+          print("my Company Name is ",Instructors.companyName)
